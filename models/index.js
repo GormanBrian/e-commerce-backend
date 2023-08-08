640109baf428a24e3fbd7475bafb94478d968996
Brian Gorman <briangorman99@gmail.com>
2023-08-08 12:40:44 -0400

    add ProductTag column definitions

--- models/index.js
+++ models/index.js
@@ -17,13 +17,13 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: "product_tag",
+  through: { model: ProductTag },
   foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: "product_tag",
+  through: { model: ProductTag },
   foreignKey: "tag_id",
 });
 
